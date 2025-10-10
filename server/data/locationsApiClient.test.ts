b6e57857b0e9c4fc32e3351e8c31ccdb2003eb33
dafhoustonmoj <dafydd.houston@justice.gov.uk>
2025-10-10 16:12:58 +0100

    MAP-2858: initial project setup

---
+++ server/data/locationsApiClient.test.ts
@@ -0,0 +1,28 @@
+import nock from 'nock'
+import type { AuthenticationClient } from '@ministryofjustice/hmpps-auth-clients'
+import { createClient } from 'redis'
+import LocationsApiClient from './locationsApiClient'
+import { redisClient } from './redisClient'
+
+describe('LocationsApiClient', () => {
+  let locationsApiClient: LocationsApiClient
+  let mockAuthenticationClient: jest.Mocked<AuthenticationClient>
+
+  beforeEach(() => {
+    mockAuthenticationClient = {
+      getToken: jest.fn().mockResolvedValue('test-system-token'),
+    } as unknown as jest.Mocked<AuthenticationClient>
+
+    locationsApiClient = new LocationsApiClient(
+      redisClient as unknown as ReturnType<typeof createClient>,
+      mockAuthenticationClient,
+    )
+  })
+
+  afterEach(() => {
+    nock.cleanAll()
+    jest.resetAllMocks()
+  })
+
+  it.skip('should test something', () => {})
+})
