1bdf8c8bead68a8300692257637cec443ce8acef
Dan Benton <daniel.benton@justice.gov.uk>
2025-11-03 12:12:42 +0000

    MAP-2874 Update locationApi mock for e2e

--- integration_tests/mockApis/locationsApi.ts
+++ integration_tests/mockApis/locationsApi.ts
@@ -19,27 +19,64 @@ export default {
     stubFor({
       request: {
         method: 'GET',
-        urlPath: `/locations-api/locations/prison/${prisonId}/non-residential`,
+        urlPath: `/locations-api/locations/non-residential/summary/${prisonId}`,
       },
       response: {
         status: 200,
         headers: {
           'Content-Type': 'application/json;charset=UTF-8',
         },
-        jsonBody: [
-          {
-            prisonId,
-            localName: 'Wing A',
-            usage: [
+        jsonBody: {
+          prisonId,
+          locations: {
+            totalElements: 0,
+            totalPages: 0,
+            size: 0,
+            content: [
               {
-                usageType: 'ADJUDICATION_HEARING',
-                capacity: 0,
-                sequence: 0,
+                id: '2475f250-434a-4257-afe7-b911f1773a4d',
+                prisonId: 'MDI',
+                localName: 'Gym',
+                code: '001',
+                pathHierarchy: 'A-1-001',
+                locationType: 'ADJUDICATION_ROOM',
+                permanentlyInactive: false,
+                permanentlyInactiveReason: 'Demolished',
+                usedByGroupedServices: ['ACTIVITIES_APPOINTMENTS'],
+                usedByServices: ['APPOINTMENT'],
+                status: 'ACTIVE',
+                deactivatedDate: '2023-01-23T12:23:00',
+                deactivatedReason: 'DAMAGED',
+                deactivationReasonDescription: 'Window damage',
+                deactivatedBy: 'string',
+                level: 1,
+                parentId: '57718979-573c-433a-9e51-2d83f887c11c',
               },
             ],
-            status: 'ACTIVE',
+            number: 0,
+            first: true,
+            last: true,
+            sort: {
+              empty: true,
+              sorted: true,
+              unsorted: true,
+            },
+            numberOfElements: 0,
+            pageable: {
+              offset: 0,
+              sort: {
+                empty: true,
+                sorted: true,
+                unsorted: true,
+              },
+              pageSize: 0,
+              paged: true,
+              pageNumber: 0,
+              unpaged: true,
+            },
+            empty: true,
           },
-        ],
+        },
       },
     }),
 }
