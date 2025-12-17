cfa5ea6e248d3a480917d798211cc4b887a2d902
Daniel Twigg <daniel.twigg@justice.gov.uk>
2025-12-17 14:51:22 +0000

    MAP-3088: Use API service labels instead of hard-coded

--- server/data/locationsApiClient.ts
+++ server/data/locationsApiClient.ts
@@ -2,7 +2,7 @@ import type { AuthenticationClient } from '@ministryofjustice/hmpps-auth-clients
 import config from '../config'
 import BaseApiClient from './baseApiClient'
 import { RedisClient } from './redisClient'
-import { Location, NonResidentialSummary } from '../@types/locationsApi/locationsApiTypes'
+import { Location, NonResidentialSummary, Constant, CompoundConstant } from '../@types/locationsApi/locationsApiTypes'
 
 export default class LocationsApiClient extends BaseApiClient {
   constructor(redisClient: RedisClient, authenticationClient: AuthenticationClient) {
@@ -32,32 +32,17 @@ export default class LocationsApiClient extends BaseApiClient {
   }
 
   constants = {
-    getNonResidentialUsageTypes: this.apiCall<
-      { nonResidentialUsageTypes: { key: string; description: string }[] },
-      null
-    >({
+    getNonResidentialUsageTypes: this.apiCall<{ nonResidentialUsageTypes: Constant[] }, null>({
       path: '/constants/non-residential-usage-type',
       requestType: 'get',
       options: { cacheDuration: 86_400 },
     }),
-    getServiceTypes: this.apiCall<
-      { nonResidentialServiceTypes: { key: string; description: string; attributes: unknown }[] },
-      null
-    >({
+    getServiceTypes: this.apiCall<{ nonResidentialServiceTypes: Constant[] }, null>({
       path: '/constants/service-types',
       requestType: 'get',
       options: { cacheDuration: 86_400 },
     }),
-    getServiceFamilyTypes: this.apiCall<
-      {
-        serviceFamilyTypes: {
-          key: string
-          description: string
-          values: { key: string; description: string; additionalInformation?: string }[]
-        }[]
-      },
-      null
-    >({
+    getServiceFamilyTypes: this.apiCall<{ serviceFamilyTypes: CompoundConstant[] }, null>({
       path: '/constants/service-family-types',
       requestType: 'get',
       options: { cacheDuration: 86_400 },
--- server/formatters/usageLabel.ts
+++
@@ -1,5 +0,0 @@
-import UsageMap from '../@types/locationsApi/usageMap'
-
-export default function usageLabel(usageType: string): string {
-  return UsageMap[usageType]?.label || usageType
-}
