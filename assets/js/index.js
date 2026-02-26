b5fd708cd035045930a3f07fbfef83031895e36c
Daniel Twigg <daniel.twigg@justice.gov.uk>
2026-02-26 20:59:46 +0000

    MAPB-264: Configure Application Insights for server-side and client-side telemetry
    Add server-side telemetry processors to attach user data to requests and
    filter health check noise. Add client-side browser tracking using
    @microsoft/applicationinsights-web with addTelemetryInitializer for page
    view and user activity telemetry.

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,7 @@
 import * as govukFrontend from 'govuk-frontend'
 import * as mojFrontend from '@ministryofjustice/frontend'
 
+import './applicationInsights'
 import './serverSideSort'
 
 govukFrontend.initAll()
