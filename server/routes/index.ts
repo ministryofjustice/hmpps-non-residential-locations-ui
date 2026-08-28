58b0d7ee86f4c8e1f325142b1d3a73b5c263eda5
Dan Freemantle <dan.freemantle@justice.gov.uk>
2026-08-28 09:56:48 +0100

    Moved page to closely match url

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -40,7 +40,7 @@ export default function routes(services: Services): Router {
   router.use('/add-location', addLocationRouter)
 
   router.get('/accessibility-statement', (req, res) =>
-    res.render('pages/help/accessibility-statement', {
+    res.render('pages/accessibility-statement', {
       serviceUrl: config.ingressUrl,
     }),
   )
